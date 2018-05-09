d6cf9177323d294eb68da49dbe6fbb963aaf9e16
Brad Irby <brad@bradirby.com>
2018-05-09 18:03:04 -0300

    Adding similar tests, called in a different way

--- StringManipulationComparison/StringManipulationComparison/Comparisons.cs
+++ StringManipulationComparison/StringManipulationComparison/Comparisons.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace StringManipulationComparison
@@ -13,99 +14,61 @@ namespace StringManipulationComparison
 
         public long  StringInterpolation(int numOps)
         {
-            var rnd = new Random();
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var lst = new List<string>();
-            for (int n = 0; n < numOps; n++)
-            {
-                var s = $"this is an int{rnd.Next(100)} this is a string {Guid.NewGuid().ToString()} and this is a date {DateTime.Now}";
-                lst.Add(s);
-            }
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            return elapsedMs;
+            string s;
+            var p1 = "1";
+            var p2 = "2";
+            var stopwatchConcat2 = Stopwatch.StartNew();
+            for (var i = 0; i < numOps; i++)
+                s = $"{p1} {p2}";
+            stopwatchConcat2.Stop();
+            return stopwatchConcat2.ElapsedMilliseconds;
         }
 
         public long StringBuilder(int numOps)
         {
-            var rnd = new Random();
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var lst = new List<string>();
-            for (int n = 0; n < numOps; n++)
+            string s;
+            var stopwatchConcat2 = Stopwatch.StartNew();
+            var theBuilder = new StringBuilder();
+            for (var i = 0; i < numOps; i++)
             {
-                var sb = new StringBuilder();
-                sb.Append("this is an int");
-                sb.Append(rnd.Next(100).ToString());
-                sb.Append("this is a string ");
-                sb.Append(Guid.NewGuid());
-                sb.Append(" and this is a date ");
-                sb.Append(DateTime.Now);
-                var s = sb.ToString();
-                lst.Add(s);
+                theBuilder.Clear();
+                theBuilder.Append("1 2");
+                s = theBuilder.ToString();
             }
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            return elapsedMs;
+
+            stopwatchConcat2.Stop();
+            return stopwatchConcat2.ElapsedMilliseconds;
+
         }
 
         public long StringFormat(int numOps)
         {
-            var rnd = new Random();
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var lst = new List<string>();
-            for (int n = 0; n < numOps; n++)
-            {
-                var s = String.Format("this is an int{0} this is a string {1} and this is a date {2}", rnd.Next(100), Guid.NewGuid().ToString(), DateTime.Now);
-                lst.Add(s);
-            }
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            return elapsedMs;
+            string s;
+            var stopwatchConcat2 = Stopwatch.StartNew();
+            for (var i = 0; i < numOps; i++)
+                s = String.Format("{0},{1}",1,2);
+            stopwatchConcat2.Stop();
+            return stopwatchConcat2.ElapsedMilliseconds;
         }
 
         public long StringAdd(int numOps)
         {
-            var rnd = new Random();
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var lst = new List<string>();
-            for (int n = 0; n < numOps; n++)
-            {
-                var s = "this is an int";
-                s += rnd.Next(100).ToString();
-                s += "this is a string ";
-                s += Guid.NewGuid().ToString();
-                s += " and this is a date ";
-                s += DateTime.Now;
-                lst.Add(s);
-            }
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            return elapsedMs;
+            string s;
+            var stopwatchConcat2 = Stopwatch.StartNew();
+            for (var i = 0; i < numOps; i++)
+                s = "1" + "2";
+            stopwatchConcat2.Stop();
+            return stopwatchConcat2.ElapsedMilliseconds;
         }
 
         public long StringConcat(int numOps)
         {
-            var rnd = new Random();
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var lst = new List<string>();
-            for (int n = 0; n < numOps; n++)
-            {
-                string[] sArr =
-                {
-                    "this is an int",
-                    rnd.Next(100).ToString(),
-                    "this is a string ",
-                    Guid.NewGuid().ToString(),
-                    " and this is a date ",
-                    DateTime.Now.ToString()
-                };
-
-                var s = string.Concat(sArr);
-                lst.Add(s);
-            }
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            return elapsedMs;
+            string s;
+            var stopwatchConcat2 = Stopwatch.StartNew();
+            for (var i = 0; i < numOps; i++)
+                s = String.Concat("1", "2");
+            stopwatchConcat2.Stop();
+            return stopwatchConcat2.ElapsedMilliseconds;
         }
     }
 }
---
+++ StringManipulationComparison/StringManipulationComparison/ComparisonsAddingToList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringManipulationComparison
+{
+    /// <summary>
+    /// Testing Note
+    ///  - strings are added to a list in order to prevent the compiler from optimizing out all logic to build the string (which would be unused)
+    /// </summary>
+    public class ComparisonsAddingToList
+    {
+
+        public long  StringInterpolation(int numOps)
+        {
+            string s;
+            var rnd = new Random();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var lst = new List<string>();
+            for (int n = 0; n < numOps; n++)
+            {
+                s = $"this is an int{rnd.Next(100)} this is a string {Guid.NewGuid().ToString()} and this is a date {DateTime.Now}";
+                lst.Add(s);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+
+        public long StringBuilder(int numOps)
+        {
+            string s;
+            var rnd = new Random();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var lst = new List<string>();
+            var sb = new StringBuilder();
+            for (int n = 0; n < numOps; n++)
+            {
+                sb.Clear();
+                sb.Append("this is an int");
+                sb.Append(rnd.Next(100).ToString());
+                sb.Append("this is a string ");
+                sb.Append(Guid.NewGuid());
+                sb.Append(" and this is a date ");
+                sb.Append(DateTime.Now);
+                s = sb.ToString();
+                lst.Add(s);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+
+        public long StringFormat(int numOps)
+        {
+            string s;
+            var rnd = new Random();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var lst = new List<string>();
+            for (int n = 0; n < numOps; n++)
+            {
+                s = String.Format("this is an int{0} this is a string {1} and this is a date {2}", rnd.Next(100), Guid.NewGuid().ToString(), DateTime.Now);
+                lst.Add(s);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+
+        public long StringAdd(int numOps)
+        {
+            string s;
+            var rnd = new Random();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var lst = new List<string>();
+            for (int n = 0; n < numOps; n++)
+            {
+                s = "this is an int";
+                s += rnd.Next(100).ToString();
+                s += "this is a string ";
+                s += Guid.NewGuid().ToString();
+                s += " and this is a date ";
+                s += DateTime.Now;
+                lst.Add(s);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+
+        public long StringConcat(int numOps)
+        {
+            string s;
+            var rnd = new Random();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var lst = new List<string>();
+            for (int n = 0; n < numOps; n++)
+            {
+                string[] sArr =
+                {
+                    "this is an int",
+                    rnd.Next(100).ToString(),
+                    "this is a string ",
+                    Guid.NewGuid().ToString(),
+                    " and this is a date ",
+                    DateTime.Now.ToString()
+                };
+
+                s = string.Concat(sArr);
+                lst.Add(s);
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            return elapsedMs;
+        }
+    }
+}
